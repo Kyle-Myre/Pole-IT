29aa02512c7263f626bcf020a41b259f71b91420
SikroxMemer <simoarmymen9@gmail.com>
2024-05-24 00:45:56 +0100

    prepering to make blog page

--- resources/views/shop/components/navbar.blade.php
+++ resources/views/shop/components/navbar.blade.php
@@ -1,3 +1,6 @@
+<head>
+    <title>Pole-IT</title>
+</head>
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow  border-5 border-primary-perso sticky-top p-0">
     <a href="index.html" class="navbar-brand navbar-logo-background d-flex align-items-center px-4 px-lg-5">
         <img src="{{url('/images/icon.png')}}" alt="Logo" style="height: 90px;"> <!-- Logo image -->
@@ -5,28 +8,19 @@
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarCollapse">
-
         <div class="navbar-nav ms-auto p-4 p-lg-0 align-items-center">
-
             <a href="{{route('home')}}" class="nav-item nav-link active">Home</a>
-
             <a href="{{route('products')}}" class="nav-item nav-link active">Products</a>
-
             @if(Auth::check())
-            <a href="{{route('logout')}}" class="nav-item nav-link">Profile</a>
             <a href="{{route('logout')}}" class="nav-item nav-link text-danger">Log out</a>
-            <a href=""></a>
             @else
             <a href="{{route('login')}}" class="nav-item nav-link">Sign up</a>
             <a href="{{route('register')}}" class="nav-item nav-link">Sign in</a>
             @endif
-
-            <a href="" class="nav-item nav-link">
-                <span><i class="bi bi-basket2 h2"></i></span>&nbsp;<span>0</span>
+            <a href="{{route('cart-list')}}" class="nav-item nav-link">
+                <span><i class="bi bi-basket2 h2"></i></span>&nbsp;<span>{{count(session()->get('cart'))}}</span>
             </a>
-
         </div>
     </div>
 </nav>
--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="{{url('/js/bootstrap.min.js')}}" defer></script>
     
-    <title>Document</title>
+    <title>Pole-IT</title>
 </head>
 
 <body>
